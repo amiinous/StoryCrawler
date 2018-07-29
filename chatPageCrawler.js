547291d9539210b2727e62591b1b6e7260b0801e
MohGanji <mohammad-ganji@outlook.com>
2018-07-29 11:17:53 +0430

    added story html generator

--- chatPageCrawler.js
+++ chatPageCrawler.js
@@ -19,6 +19,19 @@ async function run() {
       _episodes = await fs.readdirSync(
         path.join(__dirname, 'Hooked_Stories', category, story),
       )
+      const poster = JSON.parse(
+        fs.readFileSync(
+          path.join(
+            __dirname,
+            'Hooked_Stories',
+            category,
+            story,
+            'Episodes.json',
+          ),
+        ),
+      ).result.stories[0].coverImageFile.url
+
+      // console.log(poster)
       const episodes = await _episodes.filter(
         ep => ep.indexOf('_messages.json') !== -1,
       )
@@ -28,14 +41,16 @@ async function run() {
           0,
           episode.indexOf('_messages.json'),
         )
-        // console.log(episodeName)
         const episodeChatPage = await createChatPage(
           category,
           story,
           episode,
           episodeName,
+          poster,
         )
-        console.log(episodeChatPage)
+        console.log(`html created: ${category}/${story}/${episodeName}`)
+
+        // console.log(episodeChatPage)
         // await savePDF(episodeChatPage, episodeName)
       })
     })
--- chatPageCreator.js
+++ chatPageCreator.js
@@ -3,7 +3,13 @@ const fs = require('fs')
 const path = require('path')
 const template = path.join(__dirname, 'chatPageTemplate.ejs')
 
-async function create(category, story, episode, episodeName) {
+module.exports = async function create(
+  category,
+  story,
+  episode,
+  episodeName,
+  poster,
+) {
   const ejsTemplate = fs.readFileSync(template, {
     encoding: 'utf8',
   })
@@ -11,19 +17,36 @@ async function create(category, story, episode, episodeName) {
     path.join(__dirname, 'Hooked_Stories', category, story, episode),
     { encoding: 'utf8' },
   )
+  const result = JSON.parse(episodeData).result
+  let mainCharacter = ''
+  let ind = 0
+  do {
+    if (result.messages[ind].sender) {
+      mainCharacter = result.messages[ind].sender.name
+      break
+    }
+    ind += 1
+  } while (ind < result.messages.length)
 
   const html = ejs.render(ejsTemplate, {
     name: episodeName,
-    episode: JSON.parse(episodeData).result,
+    episode: result,
+    poster: poster,
+    mainCharacter,
   })
-  await fs.writeFileSync(`${__dirname}/file.html`, html, 'utf8')
-  console.log(html)
+  await fs.writeFileSync(
+    `${__dirname}/htmls/${category}_${story}_${episodeName}.html`,
+    html,
+    'utf8',
+  )
+  // console.log(html)
   // return html
 }
 
-create(
-  'Romance',
-  'Alexis the Virgin',
-  'Alexis the Virgin_messages.json',
-  'Alexis the Virgin',
-)
+// create(
+//   'Romance',
+//   'Alexis the Virgin',
+//   'Alexis the Virgin_messages.json',
+//   'Alexis the Virgin',
+//   'https://d3b7s6bg6ueiky.cloudfront.net/mfp_cd30bde4292d2220d4ecbe82784636b1281.jpg',
+// )
